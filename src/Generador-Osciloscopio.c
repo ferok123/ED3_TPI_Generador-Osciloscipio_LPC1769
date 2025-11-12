211d63401cebebf87a350dd56d644a5116b047e8
Fernando Choque <42763729+ferok123@users.noreply.github.com>
2025-11-12 19:14:07 -0300

    Update Generador-Osciloscopio.c
    descrption

--- src/Generador-Osciloscopio.c
+++ src/Generador-Osciloscopio.c
@@ -1,1 +1,11 @@
-
+/**
+ * @file Generador-Osciloscopio.c
+ * @brief Generador de funciones / Osciloscopio con LPC1769
+ *
+ * Este programa implementa un generador de señales (senoidal, cuadrada y triangular)
+ * y una función de osciloscopio que permite visualizar señales adquiridas.
+ * 
+ * @authores Choque, Fernando  Muñoz, Jose Luis  Mendoza, Marcos Julian
+ * @date Noviembre 2025
+ * @version 1.0
+ */
